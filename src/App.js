ed966c6b69b93078457b6b9b2bfb6869fa4acd39
Jason Kolenosky <jasonk58@gmail.com>
2020-03-03 23:07:18 -0800

    Cleanup routes in app.js and add a background image

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import { Router } from "@reach/router"
 import Header from './components/header.js'
+import Home from './components/home.js'
 import User from './components/user.js'
 
 function App() {
-  let Home = () => (
-    <div>
-      <Header />
-    </div>
-  )
-
-  let UserHome = () => (
-    <div>
-      <Header />
-      <User />
-    </div>
-  )
 
   return (
-    <Router>
-      <Home path="/" />
-      <UserHome path="/user" />
-    </Router>
+    <div>
+      <Header />
+      <Router>
+        <Home path="/" />
+        <User path="/user" component={ User }/>
+      </Router>
+    </div>
   );
 }
 
