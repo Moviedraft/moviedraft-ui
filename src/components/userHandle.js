2b61452a7fa99cbca02f0ac6ce60cc9aec6f6e1c
Jason Kolenosky <jasonk58@gmail.com>
2020-03-03 16:17:38 -0800

    Remove unused id

--- src/components/userHandle.js
+++ src/components/userHandle.js
@@ -57,7 +57,7 @@ class UserHandle extends Component {
       <table>
         <tbody>
           <tr>
-            <td id='userHandleCell'>
+            <td>
               {this.state.editMode ? (
                 <input
                   autoFocus
