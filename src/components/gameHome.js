235f4f1a573de3053eb5f6a3e3616e0a232f7257
Jason Kolenosky <jasonk58@gmail.com>
2020-03-23 13:40:26 -0700

    Make the rendering between game and auction better.

---
+++ src/components/gameHome.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class GameHome extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        GAME HOME!!!!
+      </div>
+    )
+  }
+}
+
+export default GameHome;
