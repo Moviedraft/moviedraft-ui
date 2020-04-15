fa1c1a6a1b1d67858c3cfc6172623a9955fbd8a2
Jason Kolenosky <jasonk58@gmail.com>
2020-04-15 12:06:56 -0700

    Added chat to game page

--- src/components/gameHome.js
+++ src/components/gameHome.js
@@ -4,6 +4,7 @@ import GamePlayers from './gamePlayers.js'
 import WeekendBoxOffice from './weekendBoxOffice.js'
 import UpcomingMovies from './upcomingMovies.js'
 import Poll from './poll.js'
+import Chat from './chat.js'
 
 class GameHome extends Component {
   constructor(props){
@@ -14,15 +15,19 @@ class GameHome extends Component {
 
   render() {
     return (
-      <div id='gameHomeDiv'>
-        <GamePlayers
-          gameId={this.props.gameId} />
-        <WeekendBoxOffice
-          gameId={this.props.gameId} />
-        <UpcomingMovies
-          gameId={this.props.gameId} />
-        <Poll
-          gameId={this.props.gameId}/>
+      <div>
+        <div id='gameHomeDiv'>
+          <GamePlayers
+            gameId={this.props.gameId} />
+          <WeekendBoxOffice
+            gameId={this.props.gameId} />
+          <UpcomingMovies
+            gameId={this.props.gameId} />
+          <Poll
+            gameId={this.props.gameId}/>
+        </div>
+        <Chat
+          gameId={this.props.gameId + '-game'}/>
       </div>
     )
   }
