e3ed336f477c4639c2766bd22ae3ee825a4298d7
Jason Kolenosky <jasonk58@gmail.com>
2020-06-23 14:20:02 -0700

    Filter movies when making the POST

--- src/components/createGame.js
+++ src/components/createGame.js
@@ -113,7 +113,7 @@ class CreateGame extends Component {
       dollarSpendingCap: this.state.auctionDollars,
       rules: rules,
       auctionItemsExpireInSeconds: this.state.auctionItemExpiryTimeSeconds,
-      movies: this.state.movies.map((movie) => movie.id),
+      movies: this.state.movies.filter(movie => (moment(movie.releaseDate).isBetween(this.state.startDate, this.state.endDate))).map((movie) => movie.id),
       startDate: moment(this.state.startDate).format(),
       endDate: moment(this.state.endDate).format(),
       gameName: this.state.gameName,
