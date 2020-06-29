4f85f3a57fb412dcade8629946bc17efadf372a0
Jason Kolenosky <jasonk58@gmail.com>
2020-06-29 13:11:57 -0700

    Apply the offset to the current time as well to make both the same

--- src/components/auctionHome.js
+++ src/components/auctionHome.js
@@ -50,8 +50,8 @@ class AuctionHome extends Component {
 
     this.setDuration()
     this.fetchCurrentUser()
-    this.fetchPlayers()
     this.getBids()
+    this.fetchPlayers()
     this.getCurrentTime()
 
     let intervalId = setInterval(() => {
@@ -243,6 +243,7 @@ class AuctionHome extends Component {
   }
 
   renderAuctionPage() {
+    console.log(this.state.bids)
     return this.props.movies.map((movie) => {
       return <AuctionItem
         key={movie.id}
