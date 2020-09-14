df804f5ece1e80d4617dbdaa56bc8d3f90ea10a2
Jason Kolenosky <jasonk58@gmail.com>
2020-09-14 11:53:34 -0700

    Get rid of blue outline on account button

--- src/components/account.js
+++ src/components/account.js
@@ -29,7 +29,7 @@ class Account extends Component {
     return (
       <div id='dropdownContainer'>
         <Dropdown>
-          <Dropdown.Toggle id='dropdown-basic' className='dropDownButton buttonOutline' variant='outline'>
+          <Dropdown.Toggle id='dropdown-basic' className='dropDownButton buttonOutline shadow-none' variant='outline'>
             <img
               id='AccountButtonProfilePic'
               src={this.props.picture ?? GenericPerson}
